17c3ff3672d45e488449bd0ac9aa276b067cc408
Nathan Vale <hireme@nathanvale.com>
2016-06-07 07:32:47 +1000

    WIP: implement signup/login/logout on the Parse Cloud

--- src/server/cloud/main.js
+++ src/server/cloud/main.js
@@ -12,47 +12,37 @@ How to use session
 var user = req.user;
 vr token  = user.getSessionToken();
 query.find(sessionToken: token);
-
-
 */
 
 Parse.Cloud.define('hello', (req, res) => {
-  res.success('boom');
+  res.success('Boom!');
 });
 
 Parse.Cloud.define('signUp', (req, res) => {
-  try {
-    const user = new Parse.User();
-    user.set('username', 'nathanvale');
-    user.set('password', 'password');
-
-    return user.signUp(null)
-      .then(
-      response => {
-        res.success(response);
-      },
-      err => {
-        console.log(res);
-        res.error(err.message);
-      });
-  } catch (err) {
-    return res.error(err.message); // -->  {code: 141, message: "asdf is not defined"}
-  }
+  const user = new Parse.User();
+  user.set('username', 'nathanvale2');
+  user.set('password', 'password');
+  return user.signUp(null)
+    .then(
+    response => res.success(response),
+    err => res.error(err.message)
+    );
 });
 
-Parse.Cloud.define('logOut', (req, res) => {
-  try {
-    const user = req.user;
-    return user.logOut()
-      .then(
-      response => {
-        res.success(response);
-      },
-      err => {
-        console.log(res);
-        res.error(err.message);
-      });
-  } catch (err) {
-    return res.error(err.message); // -->  {code: 141, message: "asdf is not defined"}
-  }
-});
+Parse.Cloud.define('logIn', (req, res) =>
+  Parse.User.logIn(
+    'nathanvale',
+    'password'
+  )
+    .then(
+    response => res.success(response),
+    err => res.error(err.message))
+);
+
+Parse.Cloud.define('logOut', (req, res) =>
+  Parse.User.logOut()
+    .then(
+    response => res.success(response),
+    err => res.error(err.message)
+    )
+);
